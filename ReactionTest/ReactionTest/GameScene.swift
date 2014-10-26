77ab0e74495143308055b1ab0e9f72d331986033
Hesham Salman <hesham8@gmail.com>
2014-10-26 13:18:16 -0400

    Began GameScene
    Began GameScene and Game Tick

---
+++ ReactionTest/ReactionTest/GameScene.swift
@@ -0,0 +1,28 @@
+//
+//  GameScene.swift
+//  ReactionTest
+//
+//  Created by Hesham Salman on 10/26/14.
+//  Copyright (c) 2014 Hesham Salman. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+let TickLengthLevelOne = NSTimeInterval(600)
+
+
+class GameScene: SKScene{
+    
+    let gameLayer = SKNode() 
+    
+    override func didMoveToView(view: SKView) {
+        self.createSceneContents()
+    }
+    
+    func createSceneContents() {
+        //Create SKSpriteNodes here and general layout for the scene
+    }
+    
+    
+}
--- ReactionTest/ReactionTest/ViewController.swift
+++ ReactionTest/ReactionTest/ViewController.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
+import SpriteKit
+
 
 class ViewController: UIViewController {
     
+    var scene: GameScene!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let skView = view as SKView
+        skView.multipleTouchEnabled = false
+        
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
     }
 
     override func didReceiveMemoryWarning() {
