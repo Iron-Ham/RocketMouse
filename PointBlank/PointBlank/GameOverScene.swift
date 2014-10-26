03d5896ddcdff73be01e42f30f38126249c3c4a4
Hesham Salman <hesham8@gmail.com>
2014-10-26 19:16:01 -0400

    Added Lose Scene, + @TODO

---
+++ PointBlank/PointBlank/GameOverScene.swift
@@ -0,0 +1,43 @@
+//
+//  GameOverScene.swift
+//  NinJump
+//
+//  Created by Hesham Salman on 10/26/14.
+//  Copyright (c) 2014 Hesham Salman. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GameOverScene: SKScene {
+    
+    init(size: CGSize, won:Bool) {
+        
+        super.init(size: size)
+        
+        backgroundColor = SKColor.whiteColor()
+        
+        var message = won ? "You Won!" : "You Lose :["
+        
+        let label = SKLabelNode(fontNamed: "Chalkduster")
+        label.text = message
+        label.fontSize = 40
+        label.fontColor = SKColor.blackColor()
+        label.position = CGPoint(x: size.width/2, y: size.height/2)
+        addChild(label)
+        
+        runAction(SKAction.sequence([
+            SKAction.waitForDuration(3.0),
+            SKAction.runBlock() {
+                let reveal = SKTransition.flipHorizontalWithDuration(0.5)
+                let scene = GameScene(size: size)
+                self.view?.presentScene(scene, transition:reveal)
+            }
+            ]))
+        
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- PointBlank/PointBlank/GameScene.swift
+++ PointBlank/PointBlank/GameScene.swift
@@ -7,6 +7,41 @@
 //
 
 import SpriteKit
+import AVFoundation
+
+var backgroundMusicPlayer: AVAudioPlayer!
+
+func playBackgroundMusic(filename: String) {
+    let url = NSBundle.mainBundle().URLForResource(
+        filename, withExtension: nil)
+    if (url == nil) {
+        println("Could not find file: \(filename)")
+        return
+    }
+    
+    var error: NSError? = nil
+    backgroundMusicPlayer =
+        AVAudioPlayer(contentsOfURL: url, error: &error)
+    if backgroundMusicPlayer == nil {
+        println("Could not create audio player: \(error!)")
+        return
+    }
+    
+    backgroundMusicPlayer.numberOfLoops = -1
+    backgroundMusicPlayer.prepareToPlay()
+    backgroundMusicPlayer.play()
+}
+
+
+
+
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let All       : UInt32 = UInt32.max
+    static let Monster   : UInt32 = 0b1       
+    static let Projectile: UInt32 = 0b10
+}
+
 
 
 func + (left: CGPoint, right: CGPoint) -> CGPoint {
@@ -44,22 +79,25 @@ extension CGPoint {
 
 
 
-class GameScene: SKScene {
+//    @TODO Give player jump ability
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    // 1
     let player = SKSpriteNode(imageNamed: "player")
     
     override func didMoveToView(view: SKView) {
-        // 2
+        
+        playBackgroundMusic("background-music-aac.caf")
         backgroundColor = SKColor.whiteColor()
-        // 3
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
-        // 4
         addChild(player)
+        physicsWorld.gravity = CGVectorMake(0,0)
+        physicsWorld.contactDelegate = self
+        
+        
         runAction(SKAction.repeatActionForever(
             SKAction.sequence([
                 SKAction.runBlock(addMonster),
-                SKAction.waitForDuration(1.0)
+                SKAction.waitForDuration(4.0)
                 ])
             ))
     }
@@ -72,17 +110,27 @@ class GameScene: SKScene {
         return random() * (max - min) + min
     }
     
+    
+//    @TODO Set monster invisible until monster reaches certain Y point
     func addMonster() {
         
         // Create sprite
         let monster = SKSpriteNode(imageNamed: "monster")
         
+        monster.physicsBody = SKPhysicsBody(rectangleOfSize: monster.size)
+        monster.physicsBody?.dynamic = true
+        monster.physicsBody?.categoryBitMask = PhysicsCategory.Monster
+        monster.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile
+        monster.physicsBody?.collisionBitMask = PhysicsCategory.None
+        
+        
+        
         // Determine where to spawn the monster along the Y axis
-        let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
+        let actualY = size.height/2
         
         // Position the monster slightly off-screen along the right edge,
         // and along a random position along the Y axis as calculated above
-        monster.position = CGPoint(x: size.width + monster.size.width/2, y: size.height/2)
+        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
         
         // Add the monster to the scene
         addChild(monster)
@@ -91,13 +139,46 @@ class GameScene: SKScene {
         let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
         
         // Create the actions
-        let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: size.height/2), duration: NSTimeInterval(actualDuration))
+        let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))
         let actionMoveDone = SKAction.removeFromParent()
         monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
- 
+        
     }
     
+    func projectileDidCollideWithMonster(projectile:SKSpriteNode, monster:SKSpriteNode) {
+        println("Hit")
+        projectile.removeFromParent()
+        monster.removeFromParent()
+    }
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        if ((firstBody.categoryBitMask & PhysicsCategory.Monster != 0) &&
+            (secondBody.categoryBitMask & PhysicsCategory.Projectile != 0)) {
+                projectileDidCollideWithMonster(firstBody.node as SKSpriteNode, monster: secondBody.node as SKSpriteNode)
+        }
+        
+    }
+    
+    
+    
+    
+    
+//    @TODO Add collision points between player and monsters. Set that to be the lose condition.
+    
     override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        
+        runAction(SKAction.playSoundFileNamed("pew-pew-lei.caf", waitForCompletion: false))
         
         // 1 - Choose one of the touches to work with
         let touch = touches.anyObject() as UITouch
@@ -106,6 +187,14 @@ class GameScene: SKScene {
         // 2 - Set up initial location of projectile
         let projectile = SKSpriteNode(imageNamed: "projectile")
         projectile.position = player.position
+        
+        projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width/2)
+        projectile.physicsBody?.dynamic = true
+        projectile.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
+        projectile.physicsBody?.contactTestBitMask = PhysicsCategory.Monster
+        projectile.physicsBody?.collisionBitMask = PhysicsCategory.None
+        projectile.physicsBody?.usesPreciseCollisionDetection = true
+        
         
         // 3 - Determine offset of location to projectile
         let offset = touchLocation - projectile.position
