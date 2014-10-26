9741071404ce5a37db2847546c0cb89ed186a834
Hesham Salman <hesham8@gmail.com>
2014-10-26 18:48:43 -0400

    Added Second Game: Ninjump
    Ninja Character on screen
    Enemies run towards ninja.
    Ninja must jump over enemies.
    Enemies appear a certain distance from the player and move at different
    speeds.

---
+++ PointBlank/PointBlank/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  PointBlank
+//
+//  Created by Hesham Salman on 10/26/14.
+//  Copyright (c) 2014 Hesham Salman. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .ResizeFill
+        skView.presentScene(scene)
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
+
