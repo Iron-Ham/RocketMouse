902cac7be23a36941ae72d3de8a6db07bafbe300
Jonathan Kissinger <kissi1j@cmich.edu>
2014-10-28 15:07:32 -0400

    Fixed syntax errors
    Still doesn't work yet

--- Server/getuser.php
+++ Server/getuser.php
@@ -10,8 +10,8 @@ if ($conn->connect_error) {
 
 $user_agent = $_SERVER['HTTP_USER_AGENT'];
 $short_ua = substr($user_agent, 0, 200);
-$uuid = 'SELECT UUID()'
-$sql = 'INSERT INTO user (uuid, user_agent) VALUES ($uuid, $short_ua)
+$uuid = 'SELECT UUID()';
+$sql = 'INSERT INTO user (uuid, user_agent) VALUES ($uuid, $short_ua)';
 
 echo $uuid;
 ?>
