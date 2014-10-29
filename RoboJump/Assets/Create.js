1a277fb34223646bc1baf6848009ceffee7fd32c
Sean Mead <mead1sm@cmich.edu>
2014-10-28 22:51:20 -0400

    Fixed RigidBodyError.  Created Forms

---
+++ RoboJump/Assets/Create.js
@@ -0,0 +1,28 @@
+﻿private static var buttonWidth = Screen.width - 16;
+private static var middle = Screen.width / 2;
+private static var buttonLeft = middle - buttonWidth/2;
+private static var fieldHeight = 30;
+private static var fieldPadding = 8;
+private static var fieldPosition = 1;
+
+static function pos(name){
+	return fieldPosition + ((fieldHeight + fieldPadding) * name);
+}
+
+static function addField(lbl, text, y){
+	return GUI.TextField(Rect(buttonLeft, y, buttonWidth, fieldHeight), text, 40);
+}
+
+static function addLabel(lbl, y){
+	GUI.Label(Rect(buttonLeft, y, buttonWidth, fieldHeight), lbl);
+}
+
+static function addRadio(grid, selStrings, row, col, y){
+	return GUI.SelectionGrid (Rect(buttonLeft, y, buttonWidth, fieldHeight*row), grid, selStrings, col);
+}
+
+static function addButton(lbl, scene, y){
+	if (GUI.Button(Rect(buttonLeft, y, buttonWidth, fieldHeight), lbl)){
+		Application.LoadLevel(scene);
+	}
+}
