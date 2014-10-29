8455ae6357c6b36d603f51a98e5ee5a7d067e908
Sean Mead <mead1sm@cmich.edu>
2014-10-29 19:47:08 -0400

    Added EndGame scene.

---
+++ RoboJump/Assets/EndGame.js
@@ -0,0 +1,33 @@
+﻿import Create;
+
+public var finalScore: GUIText;
+public var bestScore: GUIText;
+private var best: int;
+private var calculated: int;
+private var distance: int = 0;
+private var score: int = 0;
+
+function Start(){
+	calculated = 0;
+	best = 0;
+	UpdateScore();
+}
+
+function OnGUI(){
+	distance = PlayerPrefs.GetInt("distance");
+	score = PlayerPrefs.GetInt("score");
+	best = PlayerPrefs.GetInt("best");
+	calculated = distance * score;
+	if(calculated > best){
+		best = calculated;
+		PlayerPrefs.SetInt("best", best);
+	}
+	UpdateScore();
+	
+	addButton("Play", "Scene1", pos(3));
+}
+
+function UpdateScore(){
+	finalScore.text = "Score: " + distance + " * " + score + " = " + calculated;
+	bestScore.text = "Personal Best: " + best;
+}
