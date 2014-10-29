d2915d72fa900b9eca13a3afbd44871eda58392d
Sean Mead <mead1sm@cmich.edu>
2014-10-28 23:43:19 -0400

    Fixed compile issues for some devices

--- RoboJump/Assets/Create.js
+++ RoboJump/Assets/Create.js
@@ -1,11 +1,13 @@
-﻿private static var buttonWidth = Screen.width - 16;
-private static var middle = Screen.width / 2;
-private static var buttonLeft = middle - buttonWidth/2;
-private static var fieldHeight = 30;
-private static var fieldPadding = 8;
-private static var fieldPosition = 1;
+﻿import System;
 
-static function pos(name){
+private static var buttonWidth: int = Screen.width - 16;
+private static var middle: float = Screen.width / 2;
+private static var buttonLeft: float = middle - buttonWidth/2;
+private static var fieldHeight: int = 30;
+private static var fieldPadding: int  = 8;
+private static var fieldPosition: int = 1;
+
+static function pos(name: int){
 	return fieldPosition + ((fieldHeight + fieldPadding) * name);
 }
 
@@ -13,15 +15,15 @@ static function addField(lbl, text, y){
 	return GUI.TextField(Rect(buttonLeft, y, buttonWidth, fieldHeight), text, 40);
 }
 
-static function addLabel(lbl, y){
+static function addLabel(lbl: String, y: int){
 	GUI.Label(Rect(buttonLeft, y, buttonWidth, fieldHeight), lbl);
 }
 
-static function addRadio(grid, selStrings, row, col, y){
+static function addRadio(grid: int, selStrings: String[], row: int, col: int, y: int){
 	return GUI.SelectionGrid (Rect(buttonLeft, y, buttonWidth, fieldHeight*row), grid, selStrings, col);
 }
 
-static function addButton(lbl, scene, y){
+static function addButton(lbl: String, scene: String, y: int){
 	if (GUI.Button(Rect(buttonLeft, y, buttonWidth, fieldHeight), lbl)){
 		Application.LoadLevel(scene);
 	}
